47059aaa13eef5a8fcbd5f2f7ffa99d75d7aa3a1
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2019-12-26 22:19:13 +0200

    finish m1 from sololearn

---
+++ SoloLearnPython3/module_1/test_1.py
@@ -0,0 +1,17 @@
+# basic print:
+print("hello world")
+
+# power:
+print(2**5)
+
+# quotient and remainder:
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+# input:
+# x = int(input("enter a num: "))
+
+# strings:
+x = 'a2'
+print(x*4)
