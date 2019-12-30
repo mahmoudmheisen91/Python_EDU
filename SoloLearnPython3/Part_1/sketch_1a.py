6cff6049c55b9dbb916e012b4d1b6a103111e875
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2019-12-30 23:51:54 +0200

    Update sketch_1a.py

--- SoloLearnPython3/Part_1/sketch_1a.py
+++ SoloLearnPython3/Part_1/sketch_1a.py
@@ -1,8 +1,14 @@
 # Review Module 1:
+# Monty python: spam and eggs
+# foo and bar are called metasentactic variables
+
 print("Playing with Python")
 
 a = 10
 print(a)
+a += 1 # no ++ in python
+print(a)
+del a # delete a
 print("-----")
 
 # Operations:
@@ -24,7 +30,7 @@ print(4 // 2)
 print(4 // 3)
 print(2 // 4)
 print(3 // 4)
-print(4.2 // 2.2)
+print(4.2 // 2) # return float
 print("--")
 
 # Modulus/Remainder:
@@ -34,6 +40,7 @@ print("--")
 
 # Power:
 print(2**5)
+print(4 ** 0.5)
 print("-----")
 
 # Strings:
@@ -43,12 +50,59 @@ print(s1)
 print(s2)
 print(s1 + s2)
 print(s1 * 5)
+print("-----")
 
-num = input("enter a number: ")
-print(num * 3)
+print("this's is test 1")
+print('this"s is test 1')
+print("this\"s is test 1")
+print("this's \nis test \\ 1")
 
-num = int(input("enter a number: "))
-print(num * 3)
+b = """
+this is
+a docstring
+"""
+print(b)
+print(float("34"))
+print(str(23))
+
+#num = input("enter a number: ")
+#print(num * 3)
+
+#num = float(input("enter a number: "))
+#print(num * 3)
+
+b = 10
+print(f"this is b = {b}")
+print(1 + 3 + 2 * 4 / 2)
+
+print(int(1/2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
