69245480f82754069574526eb75ee02b2d7fe75c
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-02 23:55:12 +0200

    Create sketch_2a.py

---
+++ SoloLearnPython3/Part_1/sketch_2a.py
@@ -0,0 +1,19 @@
+# Logics:
+x = True # must be capital
+
+if x and 3 == 3:
+	print(str(not x))
+else:
+	print(str(False))
+
+num = 4
+if num > 10:
+	print("biiger")
+elif num > 2 or num != 5:
+	print(f"{num} bigger than 2")
+else:
+	print("nop")
+
+print(5*"-")
+
+
