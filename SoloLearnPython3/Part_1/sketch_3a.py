34033bb5550c76ff9a3f7ef4eae56f076684a3fb
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-02 23:55:17 +0200

    Create sketch_3a.py

---
+++ SoloLearnPython3/Part_1/sketch_3a.py
@@ -0,0 +1,23 @@
+# Functions:
+def my_func(x, y):
+	return x - y
+	
+print(my_func(2,3))
+
+# functions as arguments:
+def add(a, b):
+	return a + b
+	
+func = add
+print(func(1, 2))
+
+# Functions as objects:
+def add_one(x):
+	return x + 1
+	
+def do2(func,a):
+	return func(func(a))
+
+num = 4
+num2 = do2(add_one, num)
+print(num2)
