b507f874ec62e3c7df0cec357f86cd0bc8a4ef28
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-03 23:57:57 +0200

    add calculator command line project

--- SoloLearnPython3/Part_1/sketch_2b.py
+++ SoloLearnPython3/Part_1/sketch_2b.py
@@ -1,23 +1,26 @@
 # simple calculator:
 while True:
-   print("Options:")
-   print("Enter 'add' to add two numbers")
-   print("Enter 'sub' to subtract two numbers")
-   print("Enter 'mul' to multiply two numbers")
-   print("Enter 'div' to divide two numbers")
-   print("Enter 'q' to end the program")
-   user_input = input(": ")
+	print("Options:")
+	print("Enter 'add' to add two numbers")
+	print("Enter 'sub' to subtract two numbers")
+	print("Enter 'mul' to multiply two numbers")
+	print("Enter 'div' to divide two numbers")
+	print("Enter 'q' to end the program")
+	user_input = input(": ")
 
-   if user_input == "q":
-      break
-   elif user_input == "add":
-      pass
-   elif user_input == "sub":
-      pass
-   elif user_input == "mul":
-      pass
-   elif user_input == "div":
-      pass
-   else:
-      print("Unknown input")
+	if user_input == "q":
+		break
+	else:
+		num1 = float(input("enter num 1: "))
+		num2 = float(input("enter num 2: "))
+		if user_input == "add":
+			print(f"result: {num1 + num2}")
+		elif user_input == "sub":
+			print(f"result: {num1 - num2}")
+		elif user_input == "mul":
+			print(f"result: {num1 * num2}")
+		elif user_input == "div":
+			print(f"result: {num1 / num2}")
+		else:
+			print("Unknown operation")
 
