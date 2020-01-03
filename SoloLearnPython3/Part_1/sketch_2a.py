4cdce69e5199237c63a73374d6347a72a4e5af77
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-03 23:49:00 +0200

    Update sketch_2a.py

--- SoloLearnPython3/Part_1/sketch_2a.py
+++ SoloLearnPython3/Part_1/sketch_2a.py
@@ -27,3 +27,54 @@ while i < 10:
 	print(f"i = {i}")
 	
 print(10*"-")
+
+# while and else no break:
+i = 0
+while i < 3:
+	print(f"i = {i}")
+	i += 1
+else:	
+	print(10*"-")
+
+# Lists:
+li1 = []
+li2 = list()
+li3 = list([])
+
+print(f"li1 = {li1}")
+print(f"li2 = {li2}")
+print(f"li3 = {li3}")
+
+li1 = [1, 2, 3]
+print(f"li1 = {li1}")
+print(li1[0])
+
+li2 = [4, 5]
+print(f"li1+li2 = {li1+li2}")
+print(f"li1*3 = {li1 * 3}")
+print(3 in li1)
+print(3 in li2)
+print(3 not in li1)
+print(not 3 in li2)
+
+li1.append(4)
+print(len(li1))
+print(f"li1 = {li1}")
+print(li1.index(4))
+li1.insert(1, 5)
+print(f"li1 = {li1}")
+print(li1.index(4))
+
+numbers = list(range(10))
+print(numbers)
+
+numbers = list(range(5, 10))
+print(numbers)
+
+nums = list(range(3, 15, 3))
+print(nums)
+print(10*"-")
+
+# for loops:
+for i in li1:
+	print(i + "!")
