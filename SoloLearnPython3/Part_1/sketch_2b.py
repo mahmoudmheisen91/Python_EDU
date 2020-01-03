da9fe291422e70f5e7d0a9a7b0f05101d9dd0d39
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-03 23:52:29 +0200

    Create sketch_2b.py

---
+++ SoloLearnPython3/Part_1/sketch_2b.py
@@ -0,0 +1,23 @@
+# simple calculator:
+while True:
+   print("Options:")
+   print("Enter 'add' to add two numbers")
+   print("Enter 'sub' to subtract two numbers")
+   print("Enter 'mul' to multiply two numbers")
+   print("Enter 'div' to divide two numbers")
+   print("Enter 'q' to end the program")
+   user_input = input(": ")
+
+   if user_input == "q":
+      break
+   elif user_input == "add":
+      pass
+   elif user_input == "sub":
+      pass
+   elif user_input == "mul":
+      pass
+   elif user_input == "div":
+      pass
+   else:
+      print("Unknown input")
+
