20c4f0984a363b3b8bc206bc6165ad8012ee1ea7
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-06 01:01:17 +0200

    redo file opening

--- SoloLearnPython3/Part_2/sketch_4b.py
+++ SoloLearnPython3/Part_2/sketch_4b.py
@@ -1,6 +1,21 @@
 # Files:
 import os
 
-f = open(os.getcwd()+"\\text_file_1.txt", "r+")
-print(f.read())
-f.close()
+try:
+	f = open(os.getcwd()+"\\text_file_1.txt", "r+")
+	#print(f.read())
+	#print(f.read(16))
+	#print(f.readlines())
+	#for line in f:
+	#	print(line)
+	a = f.write("testing...")
+	print(a)
+	#print(f.read())
+finally:
+	f.close()
+
+
+# With - auto close:
+with open(os.getcwd()+"\\text_file_1.txt", "r+") as f:
+	print(f.read())
+
