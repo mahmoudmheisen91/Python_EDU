305caf3432a78ecf64c821e9cfa309e386ba2556
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-06 22:38:37 +0200

    Update sketch_4a.py

--- SoloLearnPython3/Part_2/sketch_4a.py
+++ SoloLearnPython3/Part_2/sketch_4a.py
@@ -1,15 +1,15 @@
 # Try except:
 try:
-	print(10)
-	print(3/0)
+    print(10)
+    print(3/0)
 except ZeroDivisionError:
-	print("error")
+    print("error")
 except (ZeroDivisionError, ValueError):
-	print("two errors")
+    print("two errors")
 except:
-	print("all")
+    print("all")
 finally:
-	print("always run")
+    print("always run")
 print(5*"-")
 
 # Assertion:
@@ -17,22 +17,24 @@ print(0)
 assert 1 == 1
 print(1)
 assert 2 == 2
-print(2) 
+print(2)
 
 try:
-	assert 2 == 1 # Assertion error
+    assert 2 == 1  # Assertion error
 except AssertionError:
-	print("Assertion Error")
+    print("Assertion Error")
 finally:
-	num = 3 # Assertion Message
-	#assert (num < 0), "num is +ve"
-	
+    num = 3  # Assertion Message
+    #assert (num < 0), "num is +ve"
+
+
 def add(num1, num2):
-	assert type(num1) == int, "num1 must be int"
-	assert type(num2) == int, "num1 must be int"
-	assert num1 >= 0, "num1 must be +ve"
-	assert num2 >= 0, "num2 must be +ve"
-	return num1 + num2
+    assert type(num1) == int, "num1 must be int"
+    assert type(num2) == int, "num1 must be int"
+    assert num1 >= 0, "num1 must be +ve"
+    assert num2 >= 0, "num2 must be +ve"
+    return num1 + num2
+
 
 #print(add(4.2, -5))
 assert add(2, 3) == 5
@@ -51,4 +53,3 @@ except:
    print("An error occurred")
    raise
 """
-
