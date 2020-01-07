fc026a47c72529d6a8f3411cb0f8bb141507642d
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-07 23:53:38 +0200

    Create sketch_5a.py

---
+++ SoloLearnPython3/Part_2/sketch_5a.py
@@ -0,0 +1,19 @@
+# Dicitionary:
+dic = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+}
+print(dic["one"])
+
+dic["four"] = 4
+dic[5] = 6
+print(dic)
+
+print(6 in dic)
+print(5 in dic)
+print(dic.get("one"))
+print(dic.get(7, "do not exists"))
+print(8*"-")
+
+# Tubles:
