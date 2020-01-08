bad59c66e2d6e1bb39220c5b23df85c6c624fe90
mahmoudmheisen91 <mahmoudmheisen91@gmail.com>
2020-01-09 01:36:08 +0200

    Create sketch_6b.py

---
+++ SoloLearnPython3/Part_2/sketch_6b.py
@@ -0,0 +1,71 @@
+# Recursion:
+from itertools import count, accumulate, takewhile
+
+
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return x * factorial(x-1)
+
+
+print(factorial(6))
+
+
+def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+
+# Need memoization (socratica)
+print(fib(4))
+
+
+def is_even(x):
+    if x == 0:
+        return True
+    else:
+        return is_odd(x-1)  # not is_even(x-1)
+
+
+def is_odd(x):
+    return not is_even(x)
+
+
+print(is_odd(17))
+print(is_even(23))
+print(8*"-")
+
+"""
+6435 Lakeview Blvd, APT#9204, Westland, MI, 
+48185 (zip code)
+"""
+
+# Sets:
+s1 = {1, 2, 2, 3, 4}
+s2 = set([11, 22, 33, 44])
+print(s1)
+print(s2)
+print(3 in s1)
+print(3 in s2)
+
+nums = {1, 2, 1, 3, 1, 4, 5, 6}
+print(nums)
+nums.add(-7)
+nums.remove(3)
+print(nums)
+nums.pop()
+print(nums)
+
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
+
+# Itertools:
